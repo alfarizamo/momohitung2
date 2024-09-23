bd6dc7f8b3ed3e1e053ee31fda8e09a4e8c10371
alfarizamo <160070893+alfarizamo@users.noreply.github.com>
2024-09-23 11:01:58 +0700

    Update momohitung-konversisuhu2.py

--- momohitung-konversisuhu2.py
+++ momohitung-konversisuhu2.py
@@ -1,36 +1,54 @@
 import streamlit as st
 
 x = st.number_input("Masukkan nilai suhu")
-
-sx = st.selectbox("Satuan awal", ["Celsius (C)", "Fahrenheit (F)", "Kelvin (K)"])
-
-sy = st.selectbox("Satuan tujuan", ["Celsius (C)", "Fahrenheit (F)", "Kelvin (K)"])
+sx = st.selectbox("Satuan awal", ["Celsius (C)", "Fahrenheit (F)", "Kelvin (K)", "Reamur (R)"])
+sy = st.selectbox("Satuan tujuan", ["Celsius (C)", "Fahrenheit (F)", "Kelvin (K)", "Reamur (R)"])
 
 sx_symbol = sx.split(" ")[-1][1]
 sy_symbol = sy.split(" ")[-1][1]
-
 y = 0
-
-if sx_symbol == 'C':
+if sx_symbol == 'C':  
     if sy_symbol == 'C':
-        y = x 
+        y = x  
     elif sy_symbol == 'F':
         y = x * 9/5 + 32 
     elif sy_symbol == 'K':
-        y = x + 273.15 
-elif sx_symbol == 'F':
+        y = x + 273.15  
+    elif sy_symbol == 'R':
+        y = x * 4/5 
+        
+elif sx_symbol == 'F':  
     if sy_symbol == 'C':
-        y = (x - 32) * 5/9  
+        y = (x - 32) * 5/9 
     elif sy_symbol == 'F':
         y = x  
     elif sy_symbol == 'K':
         y = (x - 32) * 5/9 + 273.15  
-elif sx_symbol == 'K':
+    elif sy_symbol == 'R':
+        y = (x - 32) * 4/9  
+
+elif sx_symbol == 'K': 
     if sy_symbol == 'C':
-        y = x - 273.15  
+        y = x - 273.15 
     elif sy_symbol == 'F':
-        y = (x - 273.15) * 9/5 + 32 
+        y = (x - 273.15) * 9/5 + 32  
     elif sy_symbol == 'K':
         y = x  
+    elif sy_symbol == 'R':
+        y = (x - 273.15) * 4/5 
+
+elif sx_symbol == 'R':  
+    if sy_symbol == 'C':
+        y = x * 5/4  
+    elif sy_symbol == 'F':
+        y = x * 9/4 + 32  
+    elif sy_symbol == 'K':
+        y = x * 5/4 + 273.15 
+    elif sy_symbol == 'R':
+        y = x  
+
+else:
+    st.write("Satuan yang dimasukkan tidak valid. Gunakan 'C', 'F', 'K', atau 'R'.")
+
 
 st.write(f"{x} {sx_symbol} = {y:.2f} {sy_symbol}")
